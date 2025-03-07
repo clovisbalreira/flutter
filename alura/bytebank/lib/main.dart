0f941a3dbb285ea0715e36d35dc9a3fccf548adf
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2025-03-07 08:47:21 -0300

    atualizar

---
+++ alura/bytebank/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.blueAccent[700],
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blueAccent[700],
+            foregroundColor: Colors.black,
+          ),
+        ),
+      ),
+      home: ListaTransferencia(),
+    );
+  }
+}
---
+++ alura/bytebankapi/android/app/src/main/kotlin/com/example/bytebankbancodedados/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bytebankbancodedados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
